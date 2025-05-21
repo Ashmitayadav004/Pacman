33a7328a3e74c7bd3e9c2c5ea354442880c24e2c
Ashmita Yadav <yadavashmita0408@gmail.com>
2025-05-21 11:20:37 +0530

    Initial commit

---
+++ App.java
@@ -0,0 +1,27 @@
+package Pacman;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int rowCount = 19;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("Pac Man");
+       
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        pacmanGame.requestFocus();
+        frame.setVisible(true);
+    }
+}
